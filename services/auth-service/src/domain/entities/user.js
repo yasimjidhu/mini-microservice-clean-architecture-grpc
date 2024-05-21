64aff2a4c17c7b9d0d2d6d67caa84cb5374e134e
yasimdirshad <dirshadyasim@gmail.com>
2024-05-21 17:43:53 +0530

    proto file updated

--- services/auth-service/src/domain/entities/user.js
+++ services/auth-service/src/domain/entities/user.js
@@ -1,5 +1,5 @@
 function createUser(id,userName,email,password){
-    console.log('createuser reached>>>>')
+    
     if (!userName) throw new Error('Username is required');
     if (!email) throw new Error('Email is required');
     if (!password) throw new Error('Password is required');
